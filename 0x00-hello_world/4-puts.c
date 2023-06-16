05b0b48cf0ed8cb30599d3e173f00178e2f3b6e0
chioma <chukwudichiomac@gmail.com>
2023-06-15 21:02:15 -0700

    task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - programming is like building a puzzle, task three
+ * Return 0: (Successful)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\"");
+return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammer, a piece of art, use printf function
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
