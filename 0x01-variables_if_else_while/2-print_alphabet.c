dfe89a1f6d969f1ba05c38a8b0a9c8f6d745bef2
chioma <chukwudichiomac@gmail.com>
2023-06-16 19:01:40 -0700

    task2 lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lower case alphabet
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char abt;
+
+	for (abt = 'a'; abt <= 'z'; abt++)
+	{
+		putchar(abt);
+			putchar('\n');
+	}
+	return (0);
+}
