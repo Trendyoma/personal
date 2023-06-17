fb890c36c0bf5c227329baad8cac51206962c97c
chioma <chukwudichiomac@gmail.com>
2023-06-16 18:01:17 -0700

    task 01 3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -21,11 +21,11 @@ if (n > 0)
 }
 else if (n == 0)
 {
-	printf("%d is zero", n);
+	printf("%d is zero\n", n);
 }
 else
 {
-	printf("%d is negative", n);
+	printf("%d is negative\n", n);
 }
 return (0);
 	}
