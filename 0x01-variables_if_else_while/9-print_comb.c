0d0927c88c1165161001810f7e77445eadc979bd
chioma <chukwudichiomac@gmail.com>
2023-06-16 20:28:21 -0700

    task9 add commas and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - printing numbers with coma using int
+ * Return: 0(successful)
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = '0'; z <= '9'; z++)
+	{
+		putchar(z);
+	}
+	if (z != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
