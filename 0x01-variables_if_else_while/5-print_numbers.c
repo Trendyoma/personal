a9a4ec23ff7de8f816fba56d777282903bf9bdf3
chioma <chukwudichiomac@gmail.com>
2023-06-16 19:31:11 -0700

    task5 base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing numbers from base 10
+ * Return: o(successful)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+	{
+		printf("%d\n", numb);
+	}
+	return (0);
+}
