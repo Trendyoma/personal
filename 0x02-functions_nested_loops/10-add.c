ed5ae2e76fa51cdda38dc1757579bec5eed1dbb3
chioma <chukwudichiomac@gmail.com>
2023-06-21 16:14:36 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - man entry point
+ * @g: int to be added
+ * @j: int to be added
+ * Return: result k (succesful)
+ */
+
+int add(int g, int j)
+{
+	int k;
+
+	k = g + j;
+	return (c);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,32 +6,31 @@
 
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int a, b, c;
 		for (a = 0; a <= 9; a++)
 		{
 			for (b = 0; b <= 9; b++)
 			{
-				c = a + b;
-					if (c > 9)
+				c = a * b;
+					if (b == 0)
 					{
-						d = c % 10;
-							e = (c - d) / 10;
-							_putchar(44);
-							_putchar(32);
-							_putchar(e + '0');
-							-putchar(d = '0');
-					}
-					else
-					{
-							if (y != 0)
-						{
-							_putchar(44);
-							_putchar(32);
-							_putchar(32);
-						}
 						_putchar(c + '0');
 					}
-			}
+
+					if (c <= 9 && b != 0)
+					{
+							_putchar(',');
+							_putchar(' ');
+							_putchar(' ');
+							_putchar(a + '0');
+					} else if (c >= 10)
+						{
+							_putchar(',');
+							_putchar(' ');
+							_putchar((c / 10) + '0');
+							_putchar((c % 10) + '0');
+						}
+					}
 			_putchar('\n');
 		}
 }
