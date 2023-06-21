86af10626a7f02dbfb070e4ed514a0f21d140679
chioma <chukwudichiomac@gmail.com>
2023-06-20 19:30:34 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - finding the absolute integers
+ * @h: caharacter to work with
+ * Return: 0 (successful)
+ */
+
+int _abs(int)
+{
+	if (h < 0)
+	{
+		int abs_val;
+		abs_val = h * -1;
+		return (abs_val);
+	}
+	return (h);
+}
