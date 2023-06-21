51c988fc9ecc9eeaa50fe0b05709adec16aa4518
chioma <chukwudichiomac@gmail.com>
2023-06-21 16:44:22 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_to_98 - main point of entry
+ * @n: print from this
+ * return
+ */
+
+
+void print_to_98(int n)
+{
+	int a, b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+				printf("%d, ", a);
+		}
+			else if (a == 98)
+			printf("%d\n", a);
+		}
+		else if (n >= 98)
+		{
+			for (b = n; b >= 98; b++)
+			{
+				if (b != 98)
+					printf("%d, ", b);
+				else if (b == 98)
+					printf("%d\n", b);
+			}
+		}
+}
