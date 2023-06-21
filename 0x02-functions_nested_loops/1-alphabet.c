521434f99b297d618b876affbc326f55c74bcdf3
chioma <chukwudichiomac@gmail.com>
2023-06-20 17:46:44 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - writing alphabets
+ * Return: 0 (successful)
+ */
+
+void print_alphabet(void)
+{
+	char h;
+
+	for (h = 'a'; h <= 'z'; h++)
+	{
+		_putchar(h);
+	}
+	_putchar('\n');
+
+}
