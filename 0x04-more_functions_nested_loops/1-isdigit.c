f32e4b3e22cab056662c140233ee8329a96012eb
chioma <chukwudichiomac@gmail.com>
2023-06-23 18:37:36 -0700

    task 01

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 /**
  * _isdigit - main point of entry
  * @c: numbers from 0 to 9
@@ -7,7 +7,7 @@
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
