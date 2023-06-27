f5298881243ef1f875e31e532f97e94e3d5d6723
chioma <chukwudichiomac@gmail.com>
2023-06-26 18:58:53 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - main entry point
+ * @s: character char
+ * Return: 0 success
+ */
+
+int _strlen(char *s)
+{
+	int lg = 0;
+
+	while (*s != '\0')
+	{
+		lg++;
+		s++;
+	}
+	return (lg);
+}
