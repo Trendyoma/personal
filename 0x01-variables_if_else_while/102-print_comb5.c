0c2e5291b9daf2757669f71823b3d45f7c02f030
chioma <chukwudichiomac@gmail.com>
2023-06-29 02:56:34 -0700

    task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - writing two two digit numbers
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 && b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
