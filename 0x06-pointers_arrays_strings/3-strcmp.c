a69e8c8c3ef410bbe991d6332e0009940c6ca61f
chioma <chukwudichiomac@gmail.com>
2023-06-30 08:28:44 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - main entry point
+ * @s1: first string
+ * @s2: second string
+ * @m: comparison
+ * Return: 0 suceessful
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int m;
+
+	m = strcmp(s1, s2);
+	return (m);
+}
