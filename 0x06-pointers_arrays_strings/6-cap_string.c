4075d4c328d9e9b83da69b6c03c1ef541532c2e2
chioma <chukwudichiomac@gmail.com>
2023-06-30 19:31:23 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * cap_string - main entry point
+ * @str: string
+ * Return: str successful
+ */
+char *cap_string(char *str)
+{
+	int j = 0;
+
+		while (str[j])
+		{
+			while (!(str[j] >= 'a' && str[j] <= 'z'))
+				j++;
+
+			if (str[j - 1] == ' ' ||
+					str[j - 1] == '\t' ||
+					str[j - 1] == '\n' ||
+					str[j - 1] == ',' ||
+					str[j - 1] == ';' ||
+					str[j - 1] == '.' ||
+					str[j - 1] == '!' ||
+					str[j - 1] == '?' ||
+					str[j - 1] == '"' ||
+					str[j - 1] == '(' ||
+					str[j - 1] == ')' ||
+					str[j - 1] == '{' ||
+					str[j - 1] == '}' ||
+					j == 0)
+				str[j] -= 32;
+
+
+			j++;
+		}
+	return (str);
+}
