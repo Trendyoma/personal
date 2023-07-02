18310ca6818c70a49dc5a03fe7fafc3ad40a41a9
chioma <chukwudichiomac@gmail.com>
2023-07-02 12:32:28 -0700

    last attemp at task 6PPPPPPPPPPPPPPPPPPPP

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,13 @@ void print_line(int n)
 {
 	int m;
 
-	for (m = 0; m <= n; m++)
+	for (m = 0; m < n; m++)
 	{
 		if (n <= 0)
 			_putchar('\n');
 		else
 		{
-			_putchar('-');
+			_putchar('_');
 		}
 	}
 	_putchar('\n');
