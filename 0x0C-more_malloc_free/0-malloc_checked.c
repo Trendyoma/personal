9bd48669919373b95856bc8f1c36501fcd798fb0
chioma <chukwudichiomac@gmail.com>
2023-07-15 06:56:22 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - function to check for malloc
+ * @b: size of the new memory
+ * Return: new memory pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(int) * b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
