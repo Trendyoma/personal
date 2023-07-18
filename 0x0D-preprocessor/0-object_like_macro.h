7ac3f71877c3fb8c7a6dcda3872ca013957abfd1
chioma <chukwudichiomac@gmail.com>
2023-07-18 00:18:36 -0700

    task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,5 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
-/**
- * SIZE - name of macro to be substitued
- * @1020: number for the substitution
- * Return: number for the substitution
- */
 
 #define SIZE 1024
 
