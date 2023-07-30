1375ca267619308154d87afe0715e7e9a1cfc37f
chioma <chukwudichiomac@gmail.com>
2023-07-30 10:14:37 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
+ * @ptr: pointer to the memory previously allocated by malloc
  * @old_size: size of the allocated memory for ptr
  * @new_size: new size of the new memory block
  *
@@ -10,8 +10,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr1;
-	char *old_ptr;
+	char *ptra
+	char ptrb;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -26,25 +26,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr)
 		return (malloc(new_size));
 
-	ptr1 = malloc(new_size);
-	if (!ptr1)
+	ptra = malloc(new_size);
+	if (!ptra)
 		return (NULL);
 
-	old_ptr = ptr;
+	ptrb = ptr;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
+			ptra[i] = ptrb[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
+			ptra[i] = ptrb[i];
 	}
 
 	free(ptr);
-	return (ptr1);
+	return (ptra);
 }
 
